d1f3c2b5b7503046dd67c05762afd3ed3669b831
Oduse David <89360467+KingDavidJnr@users.noreply.github.com>
2023-04-15 18:09:04 +0000

    refactored test base file

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module for test Base class """
+""" The Module for testing the Base class """
 import unittest
 from models.base import Base
 from models.square import Square
@@ -10,24 +10,24 @@ from unittest.mock import patch
 
 
 class TestBaseMethods(unittest.TestCase):
-    """ Suite to test Base class """
+    """ The Suite to testing the Base class """
 
     def setUp(self):
-        """ Method invoked for each test """
+        """ Special Method invoked for each test """
         Base._Base__nb_objects = 0
 
     def test_id(self):
-        """ Test assigned id """
+        """ The id assigned to the test """
         new = Base(1)
         self.assertEqual(new.id, 1)
 
     def test_id_default(self):
-        """ Test default id """
+        """ The Test default id """
         new = Base()
         self.assertEqual(new.id, 1)
 
     def test_id_nb_objects(self):
-        """ Test nb object attribute """
+        """ The Test-nb object attributes """
         new = Base()
         new2 = Base()
         new3 = Base()
@@ -36,7 +36,7 @@ class TestBaseMethods(unittest.TestCase):
         self.assertEqual(new3.id, 3)
 
     def test_id_mix(self):
-        """ Test nb object attributes and assigned id """
+        """ The Test nb object attributes, and the respective assigned id """
         new = Base()
         new2 = Base(1024)
         new3 = Base()
@@ -45,12 +45,12 @@ class TestBaseMethods(unittest.TestCase):
         self.assertEqual(new3.id, 2)
 
     def test_string_id(self):
-        """ Test string id """
+        """ Initialize new Test string id """
         new = Base('1')
         self.assertEqual(new.id, '1')
 
     def test_more_args_id(self):
-        """ Test passing more args to init method """
+        """ Test passing more arguments to the init method """
         with self.assertRaises(TypeError):
             new = Base(1, 1)
 
